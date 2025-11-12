13750a9d97cf3d769ee3efdd378a838c0bf10356
zacken <mchereau05@gmail.com>
2025-11-12 21:22:29 +0100

    success or not

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { success } = require('./helper.js')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
@@ -9,7 +10,8 @@ app.get('/',(req,res) => res.send('Hello, Express 2 !'))
 app.get('/api/pokemons/:id', (req,res) => {
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Vous avez demandé le pokemon ${pokemon.name}`)
+    const message = "Un pokémon a bien été trouvé."
+    res.json(success(message, pokemon))
 })
 
 app.get('/api/pokemons', (req,res) => {
